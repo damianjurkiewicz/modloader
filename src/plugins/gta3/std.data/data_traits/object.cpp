3fd41b2b30be715e00ed45b76fff29d83ffa3ec6
Damix <80942246+damianjurkiewicz@users.noreply.github.com>
2025-11-02 10:58:58 +0100

    Add SA object.dat loader hook for new entries

--- src/plugins/gta3/std.data/data_traits/object.cpp
+++ src/plugins/gta3/std.data/data_traits/object.cpp
@@ -5,11 +5,117 @@
  */
 #include <stdinc.hpp>
 #include "../data_traits.hpp"
+#include <queue>
+#include <sstream>
+#include <utility>
 using namespace modloader;
 using std::string;
 using std::tuple;
 
 static bool reading_from_readme = false;
+
+namespace
+{
+    class SaObjDatLoader
+    {
+        using loadline_hook = injector::function_hooker<0x5B5798, char*(int)>;
+        using setdir_hook = injector::function_hooker<0x5B5432, int(const char*)>;
+
+    public:
+        void Install()
+        {
+            if(installed || !gvm.IsSA())
+                return;
+
+            injector::make_static_hook<loadline_hook>([this](loadline_hook::func_type LoadLine, int file) -> char*
+            {
+                return this->HookLoadLine(LoadLine, file);
+            });
+
+            injector::make_static_hook<setdir_hook>([this](setdir_hook::func_type SetDir, const char* path) -> int
+            {
+                return this->HookSetDir(SetDir, path);
+            });
+
+            installed = true;
+        }
+
+        void Enqueue(const std::string& line)
+        {
+            if(!installed)
+                return;
+
+            std::string sanitized = line;
+            std::replace(sanitized.begin(), sanitized.end(), ',', ' ');
+
+            if(IsLineValid(sanitized))
+                store.push(std::move(sanitized));
+        }
+
+    private:
+        static constexpr const char* kEndId = "*";
+
+        bool installed = false;
+        bool eof = false;
+        std::queue<std::string> store;
+        std::string buffer;
+
+        char* HookLoadLine(loadline_hook::func_type LoadLine, int file)
+        {
+            if(!eof)
+            {
+                if(char* result = LoadLine(file))
+                {
+                    buffer.assign(result);
+                    if(!buffer.empty() && buffer[0] != '*')
+                        return const_cast<char*>(buffer.c_str());
+
+                    eof = true;
+                }
+                else
+                {
+                    eof = true;
+                }
+            }
+
+            if(!store.empty())
+            {
+                buffer = std::move(store.front());
+                store.pop();
+            }
+            else
+            {
+                buffer = kEndId;
+            }
+
+            return const_cast<char*>(buffer.c_str());
+        }
+
+        int HookSetDir(setdir_hook::func_type SetDir, const char* path)
+        {
+            eof = false;
+            return SetDir(path);
+        }
+
+        static bool IsLineValid(const std::string& line)
+        {
+            if(line.empty())
+                return false;
+
+            std::istringstream iss(line);
+            std::string modelName;
+            float f1, f2, f3, f4, f5, f6, f7;
+            int i1, i2, i3, i4, i5;
+
+            if(!(iss >> modelName >> f1 >> f2 >> f3 >> f4 >> f5 >> f6 >> f7 >> i1 >> i2 >> i3 >> i4 >> i5))
+                return false;
+
+            return true;
+        }
+    };
+
+    SaObjDatLoader g_saObjDatLoader;
+}
 
 struct object_traits : public data_traits
 {
@@ -42,7 +148,12 @@ struct object_traits : public data_traits
     template<class StoreType>
     static bool setbyline(StoreType& store, value_type& data, const gta3::section_info* section, const std::string& line)
     {
-        return data_traits::setbyline(store, data, section, line, !reading_from_readme);
+        auto success = data_traits::setbyline(store, data, section, line, !reading_from_readme);
+
+        if(success && reading_from_readme)
+            g_saObjDatLoader.Enqueue(line);
+
+        return success;
     }
 
 public: // eof_string related
@@ -62,6 +173,8 @@ REGISTER_RTTI_FOR_ANY(object_store);
 
 static auto xinit = initializer([](DataPlugin* plugin_ptr)
 {
+    g_saObjDatLoader.Install();
+
     // XXX a perfect refresh needs to set all the CBaseModelInfo::m_wObjectInfoIndex to -1 before reloading the data file
     // and clearing all bytes from CObjectData::ms_aObjectInfo[]
     auto ReloadObjectData = std::bind(injector::cstd<void(const char*, char)>::call<0x5B5360>, "data/object.dat", 0);
