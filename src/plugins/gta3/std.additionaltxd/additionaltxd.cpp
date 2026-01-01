2fa3b95ed21e21e9a921ae85d62e0ed6f89a35d2
Damix <80942246+damianjurkiewicz@users.noreply.github.com>
2026-01-01 12:46:09 +0100

    Pre-merge

--- src/plugins/gta3/std.additionaltxd/additionaltxd.cpp
+++ src/plugins/gta3/std.additionaltxd/additionaltxd.cpp
@@ -1,8 +1,1 @@
-﻿/*
- * Copyright (C) 2013-2014  LINK/2012 <dma_2012@hotmail.com>
- * Licensed under the MIT License, see LICENSE at top level directory.
- *
- * std.additionaltxd -- Additional TXD dictionaries (fastloader*)
- *
- * Ported from plugin-sdk based implementation (src-additionaltxd.cpp).
- */
+﻿;
